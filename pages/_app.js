0867bed3834945ed1d9be0f474c5a02a4119a9d1
Sina Syrous <sinasyrous@MBPsomtlhorSina.localdomain>
2021-07-22 09:38:03 +0200

    Create frontend

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,8 @@
+import Amplify from 'aws-amplify'
+import config from '../src/aws-exports'
 import '../styles/globals.css'
+
+Amplify.configure({ ...config, ssr: true })
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
