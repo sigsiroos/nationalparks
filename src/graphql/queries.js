759cfdb37b85b58c4969a85a2ecbfb2d77557971
Sina Syrous <sinasyrous@MBPsomtlhorSina.localdomain>
2021-07-22 09:37:50 +0200

    Initial setup of backend and frontend

---
+++ src/graphql/queries.js
@@ -0,0 +1,35 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const getPark = /* GraphQL */ `
+  query GetPark($id: ID!) {
+    getPark(id: $id) {
+      id
+      name
+      image {
+        bucket
+        region
+        key
+      }
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const listParks = /* GraphQL */ `
+  query ListParks(
+    $filter: ModelParkFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listParks(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        name
+        createdAt
+        updatedAt
+      }
+      nextToken
+    }
+  }
+`;
